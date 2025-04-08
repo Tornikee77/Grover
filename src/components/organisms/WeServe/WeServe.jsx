7fc92c18bf6bb7853f34cf924fdc0fdea12c2215
Rati Elizbarashvili <elizbarashvilirati15@Gmail.com>
2025-04-08 22:20:00 +0400

    serve-section id:6

--- src/components/organisms/WeServe/WeServe.jsx
+++ src/components/organisms/WeServe/WeServe.jsx
@@ -13,21 +13,21 @@ const WeServe = () => {
         </div>
         <div className={styles.cardbox}>
             <div className={styles.cards}>
-                <img src={FreeCarIcon} alt="FreeCarIcon" />
+                <img src={FreeCarIcon} alt="Free shipping" />
                 <div className={styles.cardstext}>
                     <h1>Free shipping</h1>
                     <span>Enjoy Order in a hand using the fresness of groceries</span>
                 </div>
             </div>
             <div className={styles.cards}>
-                <img src={CashIcon} alt="" />
+                <img src={CashIcon} alt="15 days returns" />
                 <div className={styles.cardstext}>
                     <h1>15 days returns</h1>
                     <span>Order in a handy way using the freshness of the groceries.</span>
                 </div>
             </div>
             <div className={styles.cards}>
-                <img src={SecureIcon} alt="" />
+                <img src={SecureIcon} alt="Secure checkout" />
                 <div className={styles.cardstext}>
                     <h1>Secure checkout</h1>
                     <span>If you get rotten items - return immediately to us.</span>
