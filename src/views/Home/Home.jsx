5fbdb7fca173e6fd9df248ec6cdc2c9eedcdfa75
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-04-13 02:15:04 +0400

    bugfix/importHeaderIn home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,23 +1,24 @@
-import Header from "../../components/organisms/Header/HEader";
+import Header from "../../components/organisms/Header/Header";
 import WeServe from "../../components/organisms/WeServe/WeServe";
-import DownlaodOurApp from "../../components/DownloadOurApp/DownlaodOurApp";
-import Footer from "../../components/organisms/Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       {/*Header*/}
       <Header />
-      {/*More than Faster*/}
-      {/*More than Faster component import here*/}
-      {/*What we Serve*/}
-      <WeServe />
-      {/*WHY CHOOSE US*/}
-      {/*WHY CHOOSE US component import here*/}
-      {/*HOME DELIVERY*/}
-      {/*HOME DELIVERY component import here*/}
-      {/*Download our app*/}
-      <DownlaodOurApp />
+      {/*How it Works*/}
+      {/*How it Works component import here*/}
+      {/*Jobs near me*/}
+      <WeServe /> {/*არვიცოდი ეს ზუსტად სად უნდა ჩამესვა*/}
+      {/*Jobs near me component import here*/}
+      {/*99+ jobs category*/}
+      {/*99+ jobs category component import here*/}
+      {/*Price value*/}
+      {/*Price value component import here*/}
+      {/*Testimonials*/}
+      {/*Testimonials near me component import here*/}
+      {/*Subscribe newsletter*/}
+      {/*Subscribe newsletter component import here*/}
       {/*Footer*/}
       <Footer />
       {/*Footer component import here*/}
