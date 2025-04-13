bd6935a6e7ed7e5a3346cb7d9d009a34596a7e2b
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-04-13 17:05:14 +0400

    bugfix/home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,5 @@
 import Header from "../../components/organisms/Header/Header";
-import WeServe from "../../components/organisms/WeServe/WeServe";
+// import WeServe from "../../components/organisms/WeServe/WeServe";
 import DownlaodOurApp from "../../components/DownloadOurApp/DownlaodOurApp";
 import Footer from "../../components/organisms/Footer/Footer";
 
@@ -14,31 +14,12 @@ const Home = () => {
       <WeServe />
       {/*WHY CHOOSE US*/}
       {/*WHY CHOOSE US component import here*/}
-      {/*HOME DELIVERY*/}
-      {/*HOME DELIVERY component import here*/}
       {/*Download our app*/}
       <DownlaodOurApp />
-      {/*What we Serve*/}
-      {/* <WeServe /> */}
-      {/*WHY CHOOSE US*/}
-      {/*WHY CHOOSE US component import here*/}
       {/*HOME DELIVERY*/}
       <Delivery />
       {/*Download our app*/}
       {/*Download our app component import here*/}
-      {/*How it Works*/}
-      {/*How it Works component import here*/}
-      {/*Jobs near me*/}
-      <WeServe /> {/*არვიცოდი ეს ზუსტად სად უნდა ჩამესვა*/}
-      {/*Jobs near me component import here*/}
-      {/*99+ jobs category*/}
-      {/*99+ jobs category component import here*/}
-      {/*Price value*/}
-      {/*Price value component import here*/}
-      {/*Testimonials*/}
-      {/*Testimonials near me component import here*/}
-      {/*Subscribe newsletter*/}
-      {/*Subscribe newsletter component import here*/}
       {/*Footer*/}
       <Footer />
       {/*Footer component import here*/}
